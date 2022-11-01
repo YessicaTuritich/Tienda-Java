7898bba98fa7ff653904e72261d9d6362f9b93a2
Yessica Turitich <yessi_turitich@hotmail.com>
2022-10-31 23:41:13 -0300

    dom

---
+++ js/product.js
@@ -0,0 +1,11 @@
+let stockProductos = [
+    {id: 1, nombre: "Sneakers DarkSouls", cantidad: 1, desc: "Footwear Sneakers Dark Souls series", precio: "US$ 185", talle: "42", img: './img/shoes-souls.jpg'},
+    {id: 2, nombre: "T-shirt DarkSouls", cantidad: 1, desc: "T-shirt DarkSouls limited edition", precio: "US$ 35", talle: "L", img: './img/ropa-1.jpg'},
+    {id: 3, nombre: "Hoodie Solaire", cantidad: 1, desc: "Hoodie Solaire, representing praise the sun", precio: "US$ 75", talle: "M", img: './img/ropa-2.jpg'},
+    {id: 4, nombre: "Hoodie DarkSouls", cantidad: 1, desc: "Hoodie Dark Souls, fighting against Iudex Gundyr", precio: "US$ 75", talle: "L", img: './img/ropa-3.jpg'},
+    {id: 5, nombre: "Hoodie Sekiro", cantidad: 1, desc: "Hoodie Sekiro, From Sekiro:Shadows die twice", precio: "US$ 75", talle: "S", img: './img/ropa-4.jpg'},
+    {id: 6, nombre: "Figure DarkSouls", cantidad: 1, desc: "Limited edition figure", precio: "US$ 225", talle: "N/A", img: './img/figure-1.jpg'},
+    {id: 7, nombre: "Figure Solaire of Astora", cantidad: 1, desc: "Limited edition figure", precio: "US$ 225", talle: "N/A", img: './img/figure-2.jpg'},
+    {id: 8, nombre: "Figure Bloodborne", cantidad: 1, desc: "Limited edition figure", precio: "US$ 225", talle: "N/A", img: './img/figure-3.jpg'},
+    {id: 9, nombre: "Figure Sekiro", cantidad: 1, desc: "Limited edition figure", precio: "US$ 225", talle: "N/A", img: './img/figure-4.jpg'},
+]
